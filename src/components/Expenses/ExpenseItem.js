befb482ca9c32a8a0e672e6d1b8d6f5f11170127
Graycemuthui <graycemuthui20@gmail.com>
2022-07-04 16:01:45 +0300

    Use arrow functions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,19 +4,17 @@ import './ExpenseItem.css';
 
 /* eslint-disable react/prop-types */
 /* eslint-disable  react/destructuring-assignment */
-function ExpenseItem(props) {
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <span className="expense-item__price">
-          $
-          {props.amount}
-        </span>
-      </div>
-    </Card>
-  );
-}
+const ExpenseItem = (props) => (
+  <Card className="expense-item">
+    <ExpenseDate date={props.date} />
+    <div className="expense-item__description">
+      <h2>{props.title}</h2>
+      <span className="expense-item__price">
+        $
+        {props.amount}
+      </span>
+    </div>
+  </Card>
+);
 
 export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,10 +2,10 @@
 /* eslint-disable  react/destructuring-assignment */
 import './Card.css';
 
-function Card(props) {
+const Card = (props) => {
   const classes = `card + ${props.className}`;
   // This is to add any value set on the className prop is added to the variable the to the div
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
