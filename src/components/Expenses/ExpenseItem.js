148dd8fec9c309fe77de82a28804ef2a7936170d
Grayce Muthui <graycemuthui20@gmail.com>
2022-07-10 23:31:26 +0300

    Use hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,29 @@
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 /* eslint-disable react/prop-types */
 /* eslint-disable  react/destructuring-assignment */
-const ExpenseItem = (props) => (
-  <Card className="expense-item">
-    <ExpenseDate date={props.date} />
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <span className="expense-item__price">
-        $
-        {props.amount}
-      </span>
-    </div>
-  </Card>
-);
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <span className="expense-item__price">${props.amount}</span>
+      </div>
+      <button type="button" onClick={clickHandler}>
+        Change Title
+      </button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
