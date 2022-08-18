a790f84810984db361f116ca8ecc7b587b3ff596
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-18 14:09:13 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/UI/Navbar";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/UI/Navbar';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const initialExpenses = [
   {
-    id: "e2",
-    title: "New TV",
+    id: 'e2',
+    title: 'New TV',
     amount: 799.49,
     date: new Date(2022, 4, 12),
   },
   {
-    id: "e3",
-    title: "Polaroid Camera",
+    id: 'e3',
+    title: 'Polaroid Camera',
     amount: 400.2,
     date: new Date(2021, 8, 3),
   },
